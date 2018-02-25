976fc247333c56e39acb21e6d2c47b3e2d48f3b6
Christian Reichel <chrstn.rchl@gmail.com>
2018-02-25 22:31:03 +0100

    Add face extractor module

--- recorder.py
+++ recorder.py
@@ -4,8 +4,11 @@
 # Version: 0.1a
 # -----------------------------------------------------
 # This script makes a directory based on an entered
-# name and a hash value and saves X images into that
-# folder as training set for object detection.
+# name and a hash value and saves X images of the 
+# user's face into that folder as training set for 
+# object detection.
+# For face detection, use Haar cascades to cut out
+# just the face.
 #
 # Algorithm description:
 # 1) Asks for a name
@@ -24,12 +27,16 @@ import cv2 as cv
 import time
 
 def recorder():
+
+    # Pretrained haar cascade classifiers.
+    face_cascade = cv.CascadeClassifier('data/cascades/haarcascade_frontalface_default.xml')
+
     # Ask for users name
     name = input("<recorder.py> How is your name?\n")
 
     # Make new dir if user not exists
     new_path_str = os.getcwd() + '/data/training/' + name + '_' + str(hash(int))
-    print('<recorder.py> Hello ' + name + '. The algorithm will now learn to recognise your face. Please rotate it slowly to make the recognition more stable. The recording start now.')
+    print('<recorder.py> Hello ' + name + '. The algorithm will now learn to recognise your face. Please don\'t rotate your face. The recording start now.')
     time.sleep(0.100)
     print('-------------------------')
     print('<recorder.py> Make new folder at ' + new_path_str)
@@ -43,27 +50,30 @@ def recorder():
 
     while output and iterator < 50:
 
-        time.sleep(0.5)
-        
-        # Resize camera image for faster recognition.
-        # Without blurring - we don't need undersampling handling at the moment.
-        camera_image = cv.resize(camera_image, (0,0), fx = 0.3, fy = 0.3)
-
         # Save grayscale image.
-        grayscale_image = cv.cvtColor(camera_image, cv.COLOR_BGR2GRAY)
+        camera_image_gray = cv.cvtColor(camera_image, cv.COLOR_BGR2GRAY)
 
-        # Write the image.
-        filename = name + str(iterator) + '.png'
-        print('<recorder.py> Save image: ' + filename)
-        cv.imwrite(new_path_str + '/' + filename, camera_image)
+        # Get face coordinates.
+        faces = face_cascade.detectMultiScale(camera_image_gray, 1.3, 5)
+
+        if len(faces) is 1:
+
+            # Cut the face out of the camera image. 
+            face_image = camera_image[faces[0][1]:faces[0][1]+faces[0][3], faces[0][0]:faces[0][0]+faces[0][2]]
+
+            # Write the image.
+            filename = name + str(iterator) + '.png'
+            print('<recorder.py> Save image: ' + filename)
+            cv.imwrite(new_path_str + '/' + filename, face_image)
+
+            # Iterate iterator.
+            iterator += 1
+
+            time.sleep(0.2)
 
         # Grab next camera image.
         output, camera_image = camera.read()
 
-        # Iterate iterator.
-        iterator += 1
-
     # End procedures.
     print('<recorder.py> Ending process. ' + str(iterator) + ' images are created and saved into ' + new_path_str + '.')
     camera.release()
-recorder()
