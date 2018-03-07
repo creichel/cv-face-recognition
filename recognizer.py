8c7e0ba38e2309e5500028d9f29799ffaaa8962b
Christian Reichel <chrstn.rchl@gmail.com>
2018-03-07 14:57:18 +0100

    Add face detector as submodule. Add time measurement for additional quality values.

--- recognizer.py
+++ recognizer.py
@@ -26,12 +26,12 @@ import os               # path finding.
 import cv2 as cv        # webcam image.
 import time             # sleep time.
 
-from modules.online_identifier import online_identifier as identifier
+from modules.online_identifier import online_identifier as classifier
 import faceUI as ui
-from modules.face_extractor import face_extractor
+from modules.face_extractor import face_extractor as extractor
+from modules.face_detector import face_detector as detector
 
 # Settings.
-path_to_trainingdata = os.getcwd() + '/data/training'
 path_to_testdata = os.getcwd() + '/data/test'
 
 probability_threshold = 0.4 # TODO: min probability threshold to distinguish known and unknown faces.
@@ -47,7 +47,7 @@ output, camera_image = camera.read()
 
 def recognize():
 
-    print('<recognizer.py recognize()> Starting recognizer ___________________________________________')
+    print('<recognizer.py recognize()> Starting recognizer _________________________________________')
     print('Saving test data as images: ' + str(save_faces))
 
     # Make folder if test faces should be saved.
@@ -59,11 +59,6 @@ def recognize():
         print('Images will be saved into this directory: ' + new_path_str)
 
     print('Image scale ratio: ' + str(camera_image_scale))
-
-    print('Classifier: ' + face_extractor.get_classifier_path())
-
-    # Load pretrained haar cascade classifiers.
-    face_cascade = cv.CascadeClassifier(face_extractor.get_classifier_path())
 
     # Get camera image.
     camera = cv.VideoCapture(0)
@@ -77,20 +72,28 @@ def recognize():
         # Without blurring - we don't need undersampling handling at the moment.
         camera_image = cv.resize(camera_image, (0,0), fx = camera_image_scale, fy = camera_image_scale)
 
-        # Save grayscale image.
-        camera_image_gray = cv.cvtColor(camera_image, cv.COLOR_BGR2GRAY)
+        # Start timer.
+        detection_time = time.time()
 
         # Get face coordinates.
-        faces = face_cascade.detectMultiScale(camera_image_gray, 1.3, 5)
+        faces, eyes, image = detector.detect_faces(camera_image, detect_eyes = False, draw_bounding_boxes = False, resize_factor = 0.2)
+
+        # Stop timer.
+        detection_time = time.time() - detection_time
 
         # Recognize every face in image.
         for (x,y,w,h) in faces:
 
             face_image = camera_image[y:y+h,x:x+w]
 
+            # Start timer.
+            class_time = time.time()
+
             # Get person and probability for captured image.
-            person, probability = identifier.identify(face_image)
-            print('Detected person: ' + person + '. Probability: ' + probability)
+            person, probability = classifier.identify(face_image)
+
+            # Stop timer.
+            class_time = time.time() - class_time
 
             # Save faces if needed.
             if save_faces:
@@ -101,10 +104,13 @@ def recognize():
                 iterator += 1
 
             # Draw data onto image.
-            camera_image = ui.labeled_box(camera_image, person, 'probability: ' + probability, (x,y), (x+w, y+h))
+            camera_image = ui.labeled_box(camera_image, person, 'prob.: ' + str(round(probability,2)), (x,y), (x+w, y+h))
 
             # Show the result.
             cv.imshow('Face recognizer', camera_image)
+
+            # Print results.
+            print('Label: ' + person + '. Probability: ' + str(probability) + '. Detection time: ' + str(round(detection_time, 2)) + 's. Class. time: ' + str(round(class_time, 2)) + 's.')
 
         # Grab next camera image.
         output, camera_image = camera.read()
